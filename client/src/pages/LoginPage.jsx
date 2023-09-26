01c8d639c824bb6f11811a612f3abfc6a995e385
Matteo Imeri <matteo.imeri@gmail.com>
2023-09-26 12:18:17 +0100

    add form login and styles

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,3 +1,14 @@
 export default function LoginPage() {
-    return "login page!!!";
+    return (
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="mb-64">
+                <h1 className="text-4xl text-center mb-4">Login</h1>
+                <form className="max-w-md mx-auto">
+                    <input type="email" placeholder="email address"></input>
+                    <input type="password" placeholder="password"></input>
+                    <button className="primary"> Login</button>
+                </form>
+            </div>
+        </div>
+    );
 }
