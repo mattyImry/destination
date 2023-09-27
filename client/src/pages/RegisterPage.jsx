d7a4d6abe2e6d72d4b8d8b86b91fec3a4f8e5a93
Matteo Imeri <matteo.imeri@gmail.com>
2023-09-27 11:37:09 +0100

    add Template to RegisterPage

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function RegisterPage() {
-    return <div>Register Page</div>;
+    return (
+        <div className="mt-4 grow flex items-center justify-around">
+            <div className="mb-64">
+                <h1 className="text-4xl text-center mb-4">Register</h1>
+                <form className="max-w-md mx-auto">
+                    <input type="email" placeholder="email address"></input>
+                    <input type="password" placeholder="password"></input>
+                    <button className="primary"> Register</button>
+                    <div className="text-center py-2 text-gray-500">
+                        Already Have An Account?
+                        <Link to={"/login"} className="underline text-black">
+                            Login
+                        </Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 }
