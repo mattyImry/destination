ab10517416f48d3e6cc0331b27864a352012d891
Matteo Imeri <matteo.imeri@gmail.com>
2023-09-27 11:40:57 +0100

    install express in api folder

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -6,6 +6,7 @@ export default function RegisterPage() {
             <div className="mb-64">
                 <h1 className="text-4xl text-center mb-4">Register</h1>
                 <form className="max-w-md mx-auto">
+                    <input type="text" placeholder="Your name.." />
                     <input type="email" placeholder="email address"></input>
                     <input type="password" placeholder="password"></input>
                     <button className="primary"> Register</button>
