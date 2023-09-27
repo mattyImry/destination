8c86df3b2d87a173c8d8de73665b60be13b8136d
Matteo Imeri <matteo.imeri@gmail.com>
2023-09-27 12:33:38 +0100

    fixed issue with cors

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
+app.use(
+    cors({
+        credential: true,
+        origin: "http://localhost:5173",
+    })
+);
 app.get("/test", (req, res) => {
     res.json("test ok");
 });
